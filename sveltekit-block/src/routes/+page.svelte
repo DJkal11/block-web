2984de190d34f5054ac7ad4a8ce6b29e6b1ff980
“Lucian” <luciankallee@gmail.com>
2022-08-30 22:19:02 +0200

    first update

---
+++ sveltekit-block/src/routes/+page.svelte
@@ -0,0 +1,116 @@
+<script>
+	
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div> 
+<section class="grid grid-cols-2 h-screen bg-white">
+	<div class=" intro-text p-20 "> 
+		<h1 class="text-left text-white text-7xl">Buy, Sell and Create
+			NFTs Online</h1>
+			<p class="text-left text-white text-2xl">Discover the hottest NFT collections, marketplace rankings,
+				and top real-time sales.
+				</p>
+
+	</div>
+	<div class="h-screen bg-white  p-20"> 
+		<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+		<lottie-player class="w-96 -mt-36 m-auto" src="https://assets5.lottiefiles.com/packages/lf20_mj9xns8a.json"  background="transparent"  speed="1"  loop autoplay></lottie-player>
+	</div>
+</section>
+<div class="grid grid-cols-3 w-9/12 h-64 absolute bottom-0 right-0"> 
+	<div class="bg-white p-10"> 
+		<svg class="w-14 h-14 ml-60" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd"></path></svg>
+		<h1 class="text-left text-3xl">Explore</h1>
+		<p class="text-lg mt-3">Browse through our marketplace.</p>
+	</div>
+	<div class="bg-orange-500 p-10"> 
+		<svg class="w-14 h-14 ml-60" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd"></path></svg>
+		<h1 class="text-left text-3xl text-white">Buy NEAR Tokens</h1>
+		<p class="text-white text-lg mt-3">Buy NEAR tokens using our exchange conversion.</p>
+	</div>
+	<div class="bg-purple-500 p-10"> 
+		<svg class="w-14 h-14 ml-60" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd"></path></svg>
+		<h1 class="text-left text-3xl text-white">Create</h1>
+		<p class="text-white text-lg mt-3">Create and mint nfts on our platform.</p>
+	</div>
+
+</div>
+
+<section class="h-screen bg-white pt-14 px-20">
+	<div class=" m-auto">
+		<div class="m-auto text-center">
+		<button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+			<span class="relative text-xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+				All Categories
+			</span>
+		  </button>
+		  <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+			<span class="relative text-xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+				Art
+			</span>
+		  </button>
+		  <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+			<span class="relative text-xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+				Video
+			</span>
+		  </button>
+		  <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+			<span class="relative text-xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+				Special Tokens
+			</span>
+		  </button>
+		  <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+			<span class="relative text-xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+				Collectibles
+			</span>
+		  </button>
+		  <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+			<span class="relative text-xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+				Photos
+			</span>
+		  </button>
+		  <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+			<span class="relative text-xl px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+				Adult
+			</span>
+		  </button>
+        </div> 
+	</div> 
+
+	<div class="flex flex-row justify-center gap-x-24  pt-20"> 
+		<div class="bg-red-300 w-96 "> 
+         <img class="w-full" alt="" src="https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1045&q=80" />
+		</div>
+		<div class=" w-96"> 
+          <h1 class="text-left">ART</h1>
+		  <p class="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+			 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+			 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			 <button class="relative mt-10 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+				<span class="relative px-5 py-2.5 transition-all ease-in duration-75 text-white font-bold dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+					View
+				</span>
+			  </button>
+		</div>
+
+	</div>
+
+</section>
+
+
+</div>
+
+<style>
+	.intro-text {
+		background-color: #16213E;
+		height: 91.2%;
+	}
+	
+</style>
